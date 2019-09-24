b9f40bbb4c5b6c0abd384f53542884a9bcfddce9
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-09-24 14:00:19 +0000

    Automatically backed up by Learn

---
+++ average.rb
@@ -0,0 +1,16 @@
+require "pry"
+
+array = [1,2,3,4]
+
+def average (array)
+  total = 0 
+  average = 0 
+  i= 0
+  array.length.times do
+    total += array[i]
+    i+=1
+  end
+ average = total / array.length
+ average
+ binding.pry
+end
---
+++ ifend.rb
@@ -0,0 +1,9 @@
+    # run_code_inside = false
+    # puts "Code before if...end"
+    # if run_code_inside
+    #  puts "code inside"
+    # end
+    # puts "Code after if...end"
+    
+    this_year = Time.now.year
+    puts "Hey, it's not 2018!" unless this_year == 2018
